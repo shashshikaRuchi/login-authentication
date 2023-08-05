cc6a90262525ae532c125c3cd1fa235d963bba23
shashshika <shashshika.ruchi@neosoftmail.com>
2023-08-05 12:08:04 +0530

    first commit

---
+++ src/components/TableConstant.js
@@ -0,0 +1,18 @@
+export const tableArray = [
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    role: "Admin",
+    type: "Full-time",
+    status: "Active",
+    id: 1,
+  },
+  {
+    name: "John Doe4",
+    email: "john@example.com",
+    role: "Admin",
+    type: "Full-time",
+    status: "Active",
+    id: 2,
+  },
+];
